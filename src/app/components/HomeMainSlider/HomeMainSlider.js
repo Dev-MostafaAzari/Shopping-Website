2e6fbc5bd1eee77b78447cc84e9d58528ed12911
Mostafa <msari808755@gmail.com>
2026-06-30 05:34:40 -0700

    HomeMainSlider

---
+++ src/app/components/HomeMainSlider/HomeMainSlider.js
@@ -0,0 +1,11 @@
+const HomeMainSlider = () => {
+    return (
+        <div className="w-full overflow-x-hidden h-[40%] bg-gray-500">
+            <div className="">
+
+            </div>
+        </div>
+    );
+}
+ 
+export default HomeMainSlider;
