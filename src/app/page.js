0b5d2b0ce6e758c3277c19f18fe3e76f43e34141
Mostafa <msari808755@gmail.com>
2026-06-30 05:34:13 -0700

    homepageResponsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import HomeMainSlider from "./components/HomeMainSlider/HomeMainSlider";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-    </>
+    <div className="w-screen overflow-x-hidden md:h-[calc(100vh-200px)] h-[calc(100vh-164px)] ">
+      <HomeMainSlider/>
+    </div>
   );
 }
