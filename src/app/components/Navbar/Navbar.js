2491054dc9f9ee8daae8b781fd7f58e3dea9f362
Mostafa <msari808755@gmail.com>
2026-06-30 05:33:09 -0700

    NavbarResponsiveFixed

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -13,17 +13,17 @@ import SubNavbar from "./SubNavbar/Subnav";
 const Navbar = () => {
     return (
         <>
-            <div className="h-[100px] hidden justify-evenly items-center bg-white md:flex ">
-                <div className="">
+            <div className="h-[100px] flex justify-evenly items-center bg-white ">
+                <div className="hidden md:block sm:hidden">
                     <Image src={Logo} width={50} height={50} alt="Logo" className="rounded-[50%]" />
                 </div>
                 <div className="">
                     <div className="relative">
-                        <input className="bg-gray-100 border-[1px] border-gray-300 w-130 rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
+                        <input className="bg-gray-100 border-[1px] border-gray-300 w-[300px] md:w-[520px] rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
                         <button className="absolute left-[5px] text-gray-400 pt-[3px] hover:text-black h-full cursor-pointer"><FontAwesomeIcon icon={faSearch} /></button>
                     </div>
                 </div>
-                <div className="flex  gap-[10px] text-gray-600 text-[20px]">
+                <div className="hidden md:flex sm:hidden  gap-[10px] text-gray-600 text-[20px]">
                     <Link href={"/"} className="hover:text-gray-700"><FontAwesomeIcon icon={faShoppingBag} /></Link>
                     <Link href={"/"} className="hover:text-gray-700"><FontAwesomeIcon icon={faHeart} /></Link>
                     <Link href={"/"} className="hover:text-gray-700" ><FontAwesomeIcon icon={faUser} /></Link>
