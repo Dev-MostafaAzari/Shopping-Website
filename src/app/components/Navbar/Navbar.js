56e2944cd57821b146247b6f6d2aa41babe037c9
Mostafa <msari808755@gmail.com>
2026-06-30 06:23:14 -0700

    navbarLogoAddedToMobileScreens

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -14,12 +14,12 @@ const Navbar = () => {
     return (
         <>
             <div className="h-[100px] flex justify-evenly items-center bg-white ">
-                <div className="hidden md:block sm:hidden">
+                <div className="block sm:hidden">
                     <Image src={Logo} width={50} height={50} alt="Logo" className="rounded-[50%]" />
                 </div>
                 <div className="">
                     <div className="relative">
-                        <input className="bg-gray-100 border-[1px] border-gray-300 w-[300px] md:w-[520px] rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
+                        <input className="bg-gray-100 border-[1px] border-gray-300 w-[200px] md:w-[520px] rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
                         <button className="absolute left-[5px] text-gray-400 pt-[3px] hover:text-black h-full cursor-pointer"><FontAwesomeIcon icon={faSearch} /></button>
                     </div>
                 </div>
