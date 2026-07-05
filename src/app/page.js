51eec689d615b6e6c4cb8e9016ed11b14c58dd12
Mostafa <msari808755@gmail.com>
2026-07-05 02:42:11 -0700

    MainPageGapAdded

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className="w-screen overflow-x-hidden md:h-[calc(100vh-200px)] h-[calc(100vh-164px)] ">
+    <div className="w-screen overflow-x-hidden md:h-[calc(100vh-200px)] h-[calc(100vh-164px)] flex flex-col gap-[20px] ">
       <HomeMainSlider/>
       <ProductCategory/>
     </div>
