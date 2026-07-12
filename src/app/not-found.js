5445a4d6f29fbee5a6ec4976058b85612b6d15e5
Mostafa <msari808755@gmail.com>
2026-07-12 00:48:56 -0700

    fontStyleChengeOnErrorPage

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 const NotFound = () => {
     return (
         <div className="w-screen overflow-x-hidden ovarflow-y-scroll lg:scrollbar-none md:h-[calc(100vh-100px)] lg:h-[calc(100vh-200px)] h-[calc(100vh-164px)] flex justify-center items-center">
-            <div className="flex flex-col justify-center items-center gap-[10px] lg:text-[20px]">
-                <Image src={error} quality={100} alt="error404" className=""/>
-                <p>متاسفانه صفحه موردنظر یافت نشد</p>
+            <div className="flex flex-col justify-center items-center gap-[10px] lg:text-[20px] p-[10px]">
+                <Image src={error} quality={100} alt="error404" loading="eager"/>
+                <p className="text-gray-600">متاسفانه صفحه موردنظر یافت نشد</p>
                 <div className="flex justify-center items-center text-blue-400">
                     <Link href={"/"} className="hover:text-blue-800">بازگشت به خانه</Link>
                 </div>
