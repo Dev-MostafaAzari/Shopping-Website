848a2e0465d51174bf0f5e74768653e571b10125
Mostafa <msari808755@gmail.com>
2026-07-13 08:52:16 -0700

    porductsFilterSubCategory

--- src/app/components/ProductsFilter/ProductsFilter.js
+++ src/app/components/ProductsFilter/ProductsFilter.js
@@ -12,7 +12,11 @@ const ProductsFilter = () => {
     const {filterShow} = useSelector(state => state.filteProduct);
     const dispatch = useDispatch();
     const [selectedCategory , setSelectedCategory]=useState(null);
-    const categoryList = [{title:"لباس",id:1},{title:"ساعت مردانه",id:2},{title:"کفش",id:3},{title:"گوشی",id:4}];
+    const categoryList = [
+        {title:"لباس",id:1,category:["ژاکت مردانه","تیشرت","پیراهن","کاپشن"]}
+        ,{title:"ساعت مردانه",id:2,category:["ساعت دیجیتال","ساعت مردانه","ساعت زنانه"]}
+        ,{title:"کفش",id:3,category:["کتانی ورزشی","کفش مجلسی","کفش زنانه","دمپایی","کفش مردانه","کفش کوهنوردی"]}
+        ,{title:"گوشی",id:4,category:["آیفون","سامسونگ","شیامی","سونی"]}];
 
     return (
         <>
@@ -22,19 +26,28 @@ const ProductsFilter = () => {
                 </div>
                 <div className="w-full h-full flex  justify-right items-center lg:hidden">
                     <button onClick={()=>{dispatch(toggleFilter());setSelectedCategory(null)}} className="mr-[5px] text-gray-600 font-thin text-[12px] border-[1px] border-solid border-gray-300 p-[5px] rounded-sm"><FontAwesomeIcon icon={faFilter}/>فیلترها</button>
-                    <motion.div initial={{opacity:0,display:"none"}} animate={filterShow ? {opacity:1,display:"flex"} : {opacity:0,display:"none"}} transition={{duration:0.3}} className="w-full h-full absolute top-[0] flex flex-col justify-start items-right bg-stone-300 z-[996] gap-[10px]">
+                    <motion.div initial={{opacity:0,display:"none"}} animate={filterShow ? {opacity:1,display:"flex"} : {opacity:0,display:"none"}} transition={{duration:0.3}} className="w-full h-full absolute top-[0] flex flex-col justify-start items-right bg-stone-300 overflow-y-scroll scrollbar-none z-[996] gap-[10px]">
                         <div className="w-full flex justify-right items-center p-[10px]">
                             <button className="text-gray-500 text-[20px]" onClick={()=>{dispatch(toggleFilter())}}><FontAwesomeIcon icon={faClose}/></button>
                         </div>
                         <div className="w-full flex justify-right items-center pr-[10px]">
-                            <div className="w-full flex flex-col gap-[10px] justify-start items-right text-[16px]">
+                            <div className="w-full flex flex-col justify-start items-right text-[16px] gap-[30px]">
                                 {categoryList.map((item)=>(
-                                    <div key={item.id} onClick={()=>{setSelectedCategory(item.id)}} className="w-full flex justify-right items-center">
-                                        <div className="w-[50%] flex justify-between items-center">
-                                            <Link className="text-slate-700" href={"/"}>{item.title}</Link>
-                                            <motion.span initial={{rotate:0}} animate={selectedCategory === item.id ? {rotate:"-90deg"} : {rotate:"0"}} transition={{duration:0.3}}><FontAwesomeIcon icon={faArrowLeft}/></motion.span>
+                                    <div className="flex flex-col justify-start items-right text-[16px] gap-[20px]" key={item.id}>
+                                        <div onClick={selectedCategory === item.id ? (()=>{setSelectedCategory(null)}) : (()=>{setSelectedCategory(item.id)})} className="w-full flex justify-right items-center">
+                                            <div className="w-[50%] flex justify-between items-center">
+                                                <Link className="text-slate-700" href={"/"}>{item.title}</Link>
+                                                <motion.span initial={{rotate:0}} animate={selectedCategory === item.id ? {rotate:"-90deg"} : {rotate:"0"}} transition={{duration:0.3}}><FontAwesomeIcon icon={faArrowLeft}/></motion.span>
+                                            </div>
                                         </div>
-                                    </div>
+                                        <motion.div initial={{display:"none",height:"0px"}} animate={selectedCategory === item.id ? {display:"grid",height:"auto"} : {display:"none",height:"0px"}} transition={{duration:0}} className="w-full grid grid-cols-4 gap-[5px]">
+                                            {item.category.map((value,index)=>(
+                                                <div key={index*Math.floor(Math.random()*10000)} className="text-slate-600 text-[12px] flex justify-center items-center">
+                                                    <Link href={"/"}>{value}</Link>    
+                                                </div>
+                                            ))}
+                                        </motion.div>
+                                    </div>  
                                 ))}
                             </div>
                         </div>
