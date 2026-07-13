baf9d6eec5f91a614bf1959e382f9c02412f11a9
Mostafa <msari808755@gmail.com>
2026-07-13 01:47:17 -0700

    ProductsPageFilter

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,19 +1,19 @@
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import AllProducts from "../components/AllProducts/AllProducts";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import ProductsFilter from "../components/ProductsFilter/ProductsFilter";
+
 
 const Products = () => {
     return (
         <div className="w-screen overflow-x-hidden ovarflow-y-scroll lg:scrollbar-none md:h-[calc(100vh-100px)] lg:h-[calc(100vh-200px)] h-[calc(100vh-164px)]">
             <div className="w-full h-full flex gap-[10px] ">
                 <div className="w-full h-full flex flex-col lg:flex-row  gap-[5px] ">
-                    <div className="flex-1 bg-blue-400">
-                        <div className="w-full h-full hidden lg:flex">
-
-                        </div>
-                        <div className="w-full h-full flex justify-center items-center lg:hidden">
-                            filterbar
-                        </div>
+                    <div className="flex-1 p-[10px]">
+                        <ProductsFilter/>
                     </div>
-                    <div className="flex-4 bg-gray-300 overflow-y-scroll scrollbar-none overflow-x-hidden lg:flex-3">
+                    <div className="flex-5 bg-gray-300 overflow-y-scroll scrollbar-none overflow-x-hidden lg:flex-3">
                         <AllProducts/>
                     </div>
                 </div>
