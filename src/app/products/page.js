974db34968a71daf507c18598c66624574814c43
Mostafa <msari808755@gmail.com>
2026-07-13 07:51:06 -0700

    ProductsPageDisplayChange

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,14 +1,13 @@
-
 import AllProducts from "../components/AllProducts/AllProducts";
 import ProductsFilter from "../components/ProductsFilter/ProductsFilter";
 
 
 const Products = () => {
     return (
-        <div className="w-screen overflow-x-hidden ovarflow-y-scroll lg:scrollbar-none md:h-[calc(100vh-100px)] lg:h-[calc(100vh-200px)] h-[calc(100vh-164px)]">
+        <div className="w-screen overflow-x-hidden ovarflow-y-scroll lg:scrollbar-none md:h-[calc(100vh-100px)] lg:h-[calc(100vh-200px)] h-[calc(100vh-164px)] relative">
             <div className="w-full h-full flex gap-[10px] ">
                 <div className="w-full h-full flex flex-col lg:flex-row  gap-[5px] ">
-                    <div className="flex-1 p-[10px]">
+                    <div className="flex-1">
                         <ProductsFilter/>
                     </div>
                     <div className="flex-5 bg-gray-300 overflow-y-scroll scrollbar-none overflow-x-hidden lg:flex-3">
