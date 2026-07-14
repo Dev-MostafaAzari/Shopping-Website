29a6c85fd363eae25b85809ca52297726f3800ea
Mostafa <msari808755@gmail.com>
2026-07-14 07:02:46 -0700

    categoryFontSizeOnLgScreens

--- src/app/components/ProductsFilter/ProductsFilter.js
+++ src/app/components/ProductsFilter/ProductsFilter.js
@@ -29,11 +29,11 @@ const ProductsFilter = () => {
                         <div className="w-[80%] h-full flex flex-col justify-start items-center gap-[30px] p-[10px]">
                             {categoryList.map((item)=>(
                                 <div key={item.id} className="w-full flex flex-col justify-center items-right">
-                                    <div className="w-full flex justify-between items-center cursor-pointer shadow-md shadow-zinc-400 p-[10px] rounded-xl" onClick={selectedCategory === item.id ? (()=>{setSelectedCategory(null)}) : (()=>{setSelectedCategory(item.id)})}>
+                                    <div className="w-full flex justify-between items-center cursor-pointer shadow-md shadow-zinc-400 p-[10px] rounded-xl lg:text-[12px] xl:text-[16px]" onClick={selectedCategory === item.id ? (()=>{setSelectedCategory(null)}) : (()=>{setSelectedCategory(item.id)})}>
                                         <Link className="text-slate-600 hover:text-slate-900 cursor-pointer" href={"/"}>{item.title}</Link>
                                         <motion.span initial={{rotate:"0"}} animate={selectedCategory === item.id ? {rotate:"-90deg"} : {rotate:"0"}} transition={{duration:"0.3"}}><FontAwesomeIcon icon={faAngleDoubleLeft}/></motion.span>
                                     </div>
-                                    <motion.div  className="w-full grid grid-cols-1 xl:grid-cols-2 gap-[10px] p-[10px] overflow-hidden" initial={{height:"0px"}} animate={selectedCategory === item.id ? {height:"auto"} : {height:"0px"}} transition={{ease:"easeInOut",duration:0.1}}>  
+                                    <motion.div  className="w-full grid grid-cols-1 xl:grid-cols-2 gap-[10px] p-[10px] overflow-hidden lg:text-[12px]" initial={{height:"0px"}} animate={selectedCategory === item.id ? {height:"auto"} : {height:"0px"}} transition={{ease:"easeInOut",duration:0.1}}>  
                                         {item.category.map((value,index)=>(
                                             <motion.div key={index*Math.floor(Math.random()*1000000)} initial={{display:"none"}} animate={selectedCategory === item.id ? {display:"flex"} : {display:"none"}} transition={{damping:"6000",type:"spring"}} className="flex justify-center items-center">
                                                 <Link className="text-slate-500 hover:text-slate-700" href={"/"}>{value}</Link>
