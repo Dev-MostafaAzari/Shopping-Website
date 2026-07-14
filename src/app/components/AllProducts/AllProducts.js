5eacb5664fc83313d603921cd8bb86f5997ef030
Mostafa <msari808755@gmail.com>
2026-07-14 06:55:14 -0700

    AllProductsFirstStyle

--- src/app/components/AllProducts/AllProducts.js
+++ src/app/components/AllProducts/AllProducts.js
@@ -3,7 +3,13 @@ import productsApi from "@/app/services/axios";
 
 const AllProducts = () => {
     return (
-        <div>allproducs</div>
+        <div className="w-full h-full">
+            <div className="w-full h-full p-[10px]">
+                <div className="w-full h-full rounded-md shadow-sm shadow-zinc-400">
+                    
+                </div>
+            </div>
+        </div>
     );
 }
  
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -10,7 +10,7 @@ const Products = () => {
                     <div className="flex-1">
                         <ProductsFilter/>
                     </div>
-                    <div className="flex-5 bg-gray-300 overflow-y-scroll scrollbar-none overflow-x-hidden lg:flex-3">
+                    <div className="flex-5 lg:flex-3">
                         <AllProducts/>
                     </div>
                 </div>
