776bd7020e889f8dd284f6eee54fa81a678362be
Mostafa <msari808755@gmail.com>
2026-07-18 08:09:47 -0700

    AllProductsSliceNewReducersAdded

--- src/app/redux/features/allProducsSlice.js
+++ src/app/redux/features/allProducsSlice.js
@@ -8,13 +8,16 @@ const AllProductsSlice = createSlice({
     initialState , 
     reducers : {
         nextPage : (state) => {
-            state.skip += 10;
+            state.skip += 15;
         },
         prevPage : (state) => {
-            state.skip -=10
+            state.skip -=15
+        },
+        setPage : (state,action)=>{
+            state.skip = action.payload * 15;
         }
     }
 })
 
 export default AllProductsSlice.reducer;
-export const {nextPage , prevPage} = AllProductsSlice.actions;
+export const {nextPage , prevPage , setPage} = AllProductsSlice.actions;
