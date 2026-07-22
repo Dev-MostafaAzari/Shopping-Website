32d78b73385b75ab4f6ea553d1b7fca7e3f5dda1
Mostafa <msari808755@gmail.com>
2026-07-22 09:03:07 -0700

    QuantitySelectorChangeValueFixed

--- src/app/components/QuantitySelector/QuantitySelector.js
+++ src/app/components/QuantitySelector/QuantitySelector.js
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 const QuantitySelector = () => {
-    const [quantity , setQuantity] = useState(0);
+    const [quantity , setQuantity] = useState(1);
     return (
         <div className="flex justify-center items-center gap-[5px]">
             <button className={`p-[10px] text-gray-600 text-[16px] `} onClick={()=>{setQuantity(prev=>prev+1)}}><FontAwesomeIcon icon={faAngleRight}/></button>
-            <input className="p-[10px]" type="number" value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}/>
-            <button className={`p-[10px] text-[16px] ${quantity===0 ? "text-gray-300" : "text-gray-600"}`} disabled={quantity === 0 ? true : false} onClick={quantity !=0 ? ()=>{setQuantity(prev=>prev-1)} : null }><FontAwesomeIcon icon={faAngleLeft}/></button>
+            <input className="p-[10px] w-[50px] text-center outline-none" type="number" value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}/>
+            <button className={`p-[10px] text-[16px] ${quantity=== 1 ? "text-gray-300" : "text-gray-600"}`} disabled={quantity === 1 ? true : false} onClick={quantity !=1 ? ()=>{setQuantity(prev=>prev-1)} : null }><FontAwesomeIcon icon={faAngleLeft}/></button>
         </div>
     );
 }
