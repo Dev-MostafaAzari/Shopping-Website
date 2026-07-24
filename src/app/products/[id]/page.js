07568ace36866642d39f3dd0d6b6ce65da9d5ab3
Mostafa <msari808755@gmail.com>
2026-07-24 06:10:20 -0700

    SingleProductImageSliderBugfixedAndResponsiveOnMdAndLgScreens

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -28,6 +28,7 @@ const SingleProduct = ({params}) => {
         }
         GetProduct();
     },[])
+    console.log(product?.images.length)
     return (
         <div className="w-screen overflow-x-hidden overflow-y-scroll lg:scrollbar-none md:h-[calc(100vh-100px)] lg:h-[calc(100vh-200px)] h-[calc(100vh-164px)]">
             <div className="w-full h-full p-[10px]">
@@ -38,18 +39,18 @@ const SingleProduct = ({params}) => {
                                 <div className="w-full h-full flex flex-col gap-[5px]">
                                     <div className="flex-3 flex justify-center items-center relative">
                                         <div className="w-[200px] lg:w-[400px] xl:w-[500px] h-full overflow-hidden">
-                                            <motion.div  animate={width >=1280 ? {translateX:slide*500} : (width >=1024 ? {translateX:slide*400} : {translateX:slide*200})} transition={{duration:0.5}} className={`h-full w-[600px] lg:w-[1200px] xl:w-[1500px] flex`}>
+                                            <motion.div style={width >=1280 ? {width:`${product?.images.length*500}px`} : width>=1024 ? {width:`${product?.images.length*400}px`} : {width:`${product?.images.length*200}px`}}  animate={width >=1280 ? {translateX:slide*500} : (width >=1024 ? {translateX:slide*400} : {translateX:slide*200})} transition={{duration:0.5}} className={`h-full flex`}>
                                                 {product?.images.map((item,index)=>(
                                                     <div key={index} className="p-[10p] w-[200px] lg:w-[400px] xl:w-[500px] h-full flex justify-center items-center">
-                                                        <Image key={index} src={item} alt="productPic" quality={100} width={100} height={100} className="w-full h-auto"/>
+                                                        <Image key={index} src={item} alt="productPic" quality={100} width={300} height={100} className="w-[200px] lg:w-[400px] xl:w-[500px] h-auto"/>
                                                     </div>
                                                 ))}
                                             </motion.div>
                                         </div>
-                                        <button className="absolute top-[10px] left-[10px] text-gray-400 text-[16px] cursor-pointer"><FontAwesomeIcon icon={faHeart}/></button> {/* reminder : add to favorite in progress ... */}
+                                        <button className="absolute top-[10px] left-[10px] xl:left-[20px] 2xl:left-[50px] text-gray-400 text-[16px] cursor-pointer"><FontAwesomeIcon icon={faHeart}/></button> {/* reminder : add to favorite in progress ... */}
                                     </div>
                                     <div className="flex-1">
-                                        <div className="w-full h-full flex justify-center items-center gap-[10px] ">
+                                        <div className="w-full h-full flex justify-center items-center gap-[10px] overflow-hidden">
                                             {product?.images.map((item,index)=>(
                                                 <div key={index} onClick={()=>{setSlide(index)}} className={`p-[20p] cursor-pointer rounded-xl ${slide===index ? "border-[1px] border-gray-600":""}`}>
                                                     <Image key={index} src={item} alt="productPic" quality={100} width={100} height={100} className="w-[75px] h-auto"/>
@@ -59,8 +60,9 @@ const SingleProduct = ({params}) => {
                                     </div>
                                 </div>
                             </div>
+                            {/* product info */}
                             <div className="flex-1">
-                                <div className="w-full h-full flex flex-col gap-[10px]">
+                                <div className="w-full h-full flex flex-col gap-[10px] xl:w-[70%] xl:gap-[15px] 2xl:w-[60%] 2xl:items-center 2xl:gap-[20px]">
                                     <div className="w-full flex justify-right items-center text-zinc-800 text-[20px]">
                                         <h1>{product?.title}</h1>
                                     </div>
@@ -74,9 +76,9 @@ const SingleProduct = ({params}) => {
                                     <div className="w-full flex justify-right items-center text-pretty text-[16px] text-zinc-400" dir="ltr">
                                         <p>{product?.description}</p>
                                     </div>
-                                    <div className="w-full h-full flex gap-[10px] justify-between p-[10px]">
+                                    <div className="w-full h-full flex gap-[10px] justify-between md:justify-around p-[10px]">
                                         <div className="flex items-center">
-                                            <button className="text-stone-50 text-[16px] border-1px rounded-xl bg-sky-700 p-[10px]">افزودن به سبدخرید</button>
+                                            <button className="text-stone-50 text-[16px] border-1px rounded-xl bg-sky-700 p-[10px] cursor-pointer hover:bg-sky-900">افزودن به سبدخرید</button>
                                         </div>
                                         <div className="flex p-[10px] items-center ">
                                             <QuantitySelector/>
@@ -90,13 +92,13 @@ const SingleProduct = ({params}) => {
                     <div className="flex-1">
                         <div className="w-full flex flex-col gap-[10px]">
                             <div className="flex w-full justify-evenly items-center">
-                                <motion.button onClick={()=>{setActiveTap(0)}} className={`p-[10px] ${activeTab===0 ? "text-gray-700":"text-gray-400"} relative`}>مشخصات 
+                                <motion.button onClick={()=>{setActiveTap(0)}} className={`p-[10px] ${activeTab===0 ? "text-gray-700":"text-gray-400 cursor-pointer"} relative`}>مشخصات 
                                     <motion.div initial={{width:"0px",height:"2px"}} animate={activeTab === 0 ? {width:"100%",height:"2px"} : null} transition={{direction:0.5}} className="absolute bottom-[0px] right-[0px] bg-blue-600 rounded-xl"/>
                                 </motion.button>
-                                <motion.button onClick={()=>{setActiveTap(1)}} className={`p-[10px] ${activeTab===1 ? "text-gray-700":"text-gray-400"} relative`}>جزئیات محصول 
+                                <motion.button onClick={()=>{setActiveTap(1)}} className={`p-[10px] ${activeTab===1 ? "text-gray-700":"text-gray-400 cursor-pointer"} relative`}>جزئیات محصول 
                                     <motion.div initial={{width:"0px",height:"2px"}} animate={activeTab === 1 ? {width:"100%",height:"2px"} : null} transition={{direction:0.5}} className="absolute bottom-[0px] right-[0px] bg-blue-600 rounded-xl"/>
                                 </motion.button>
-                                <motion.button onClick={()=>{setActiveTap(2)}} className={`p-[10px] ${activeTab===2 ? "text-gray-700":"text-gray-400"} relative`}>نظرات 
+                                <motion.button onClick={()=>{setActiveTap(2)}} className={`p-[10px] ${activeTab===2 ? "text-gray-700":"text-gray-400 cursor-pointer"} relative`}>نظرات 
                                     <span>{`(${product?.reviews.length})`}</span> <motion.div initial={{width:"0px",height:"2px"}} animate={activeTab === 2 ? {width:"100%",height:"2px"} : null} transition={{direction:0.5}} className="absolute bottom-[0px] right-[0px] bg-blue-600 rounded-xl"/>
                                 </motion.button>
                             </div>
@@ -133,16 +135,16 @@ const SingleProduct = ({params}) => {
                                 </div>
                             </div>
                             {/* reviews part*/}               
-                            <div className={`w-full p-[10px] gap-[10px] ${activeTab === 2 ? "flex flex-col" : "hidden"}`}>
+                            <div className={`w-full p-[10px] gap-[10px] md:gap-[15px] ${activeTab === 2 ? "flex flex-col" : "hidden"}`}>
                                 {product?.reviews.map((item,index)=>(
                                     <div key={index} className="w-full p-[10px]">
                                         <div className="w-full flex justify-right gap-[10px] relative">
                                             <p className="text-gray-800">{item.reviewerName}</p>
                                             <div className="flex gap-[3px]" dir="ltr">{item.rating}<span className="text-amber-400"><FontAwesomeIcon icon={faStar}/></span></div>
-                                            <div className="absolute top-[5px] left-[5px] text-gray-700 text-[8px]">{item.date}</div>
+                                            <div className="absolute top-[5px] left-[5px] text-gray-700 text-[8px] md:text-[12px]">{item.date}</div>
                                         </div>
-                                        <div className="w-full flex text-gray-500 text-pretty text-[12px]" dir="ltr">
-                                            <p>{item.comment}</p>
+                                        <div className="w-full flex items-center text-gray-500 text-pretty text-[12px] md:text-[16px]" >
+                                            <p dir="ltr">{item.comment}</p>
                                         </div>
                                     </div>
                                 ))}
