8e60bf2e7ce5b11391542ee8469c5420a454d75c
Mostafa <msari808755@gmail.com>
2026-07-24 05:04:53 -0700

    SingleProductTabs

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -75,7 +75,7 @@ const SingleProduct = ({params}) => {
                                         <p>{product?.description}</p>
                                     </div>
                                     <div className="w-full h-full flex gap-[10px] justify-between p-[10px]">
-                                        <div className="flex p-[10px] items-center">
+                                        <div className="flex items-center">
                                             <button className="text-stone-50 text-[16px] border-1px rounded-xl bg-sky-700 p-[10px]">افزودن به سبدخرید</button>
                                         </div>
                                         <div className="flex p-[10px] items-center ">
@@ -86,13 +86,14 @@ const SingleProduct = ({params}) => {
                             </div>
                         </div>
                     </div>
+                    {/* taps */}
                     <div className="flex-1">
                         <div className="w-full flex flex-col gap-[10px]">
                             <div className="flex w-full justify-evenly items-center">
-                                <motion.button onClick={()=>{setActiveTap(0)}} className={`p-[10px] ${activeTab===0 ? "text-gray-700":"text-gray-400"} relative`}>جزئیات محصول 
+                                <motion.button onClick={()=>{setActiveTap(0)}} className={`p-[10px] ${activeTab===0 ? "text-gray-700":"text-gray-400"} relative`}>مشخصات 
                                     <motion.div initial={{width:"0px",height:"2px"}} animate={activeTab === 0 ? {width:"100%",height:"2px"} : null} transition={{direction:0.5}} className="absolute bottom-[0px] right-[0px] bg-blue-600 rounded-xl"/>
                                 </motion.button>
-                                <motion.button onClick={()=>{setActiveTap(1)}} className={`p-[10px] ${activeTab===1 ? "text-gray-700":"text-gray-400"} relative`}>مشخصات 
+                                <motion.button onClick={()=>{setActiveTap(1)}} className={`p-[10px] ${activeTab===1 ? "text-gray-700":"text-gray-400"} relative`}>جزئیات محصول 
                                     <motion.div initial={{width:"0px",height:"2px"}} animate={activeTab === 1 ? {width:"100%",height:"2px"} : null} transition={{direction:0.5}} className="absolute bottom-[0px] right-[0px] bg-blue-600 rounded-xl"/>
                                 </motion.button>
                                 <motion.button onClick={()=>{setActiveTap(2)}} className={`p-[10px] ${activeTab===2 ? "text-gray-700":"text-gray-400"} relative`}>نظرات 
@@ -100,9 +101,52 @@ const SingleProduct = ({params}) => {
                                 </motion.button>
                             </div>
                             <hr className="text-gray-300"/>
-                                
-                            
-                                            
+                            <div className={`w-full p-[10px] ${activeTab===0 || activeTab===1 ? "flex flex-col" : "hidden"}`}>
+                                <div className="w-full p-[10px]">
+                                    {activeTab===0 ? <div className="w-full flex justify-right items-center gap-[5px]"><p className="text-zinc-800">نام محصول</p>:<p className="text-gray-500">{product?.title}</p></div>
+                                        : 
+                                    activeTab===1 ? <div className="w-full flex justify-right items-center gap-[5px]"><p className="text-zinc-800">وزن</p>:<p className="text-gray-500">{product?.weight}kg</p></div> : null}
+                                </div>
+                                <div className="w-full p-[10px]">
+                                    {activeTab === 0 ?
+                                        <div className="w-full flex justify-right items-center gap-[5px]">
+                                            <p className="text-zinc-800">برند</p>:<p className="text-gray-500">{product?.brand}</p>
+                                        </div>
+                                    : null}
+                                    {activeTab === 1 ?
+                                        <div className="w-full flex justify-right items-center gap-[5px]">
+                                            <p className="text-zinc-800">ابعاد</p>:<p className="text-gray-500">{product?.dimensions.width}x{product?.dimensions.height}x{product?.dimensions.depth}</p>
+                                        </div>
+                                    : null}
+                                </div>
+                                <div className="w-full  p-[10px]">
+                                    {activeTab === 0 ?
+                                        <div className="w-full flex justify-right items-center gap-[5px]">
+                                            <p className="text-zinc-800">دسته بندی</p>:<p className="text-gray-500">{product?.category}</p>
+                                        </div>
+                                    : null}
+                                    {activeTab === 1 ?
+                                        <div className="w-full flex justify-right items-center gap-[5px]">
+                                            <p className="text-zinc-800">گارانتی</p>:<p className="text-gray-500">{product?.warrantyInformation}</p>
+                                        </div>
+                                    : null}
+                                </div>
+                            </div>
+                            {/* reviews part*/}               
+                            <div className={`w-full p-[10px] gap-[10px] ${activeTab === 2 ? "flex flex-col" : "hidden"}`}>
+                                {product?.reviews.map((item,index)=>(
+                                    <div key={index} className="w-full p-[10px]">
+                                        <div className="w-full flex justify-right gap-[10px] relative">
+                                            <p className="text-gray-800">{item.reviewerName}</p>
+                                            <div className="flex gap-[3px]" dir="ltr">{item.rating}<span className="text-amber-400"><FontAwesomeIcon icon={faStar}/></span></div>
+                                            <div className="absolute top-[5px] left-[5px] text-gray-700 text-[8px]">{item.date}</div>
+                                        </div>
+                                        <div className="w-full flex text-gray-500 text-pretty text-[12px]" dir="ltr">
+                                            <p>{item.comment}</p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>                
                         </div>
                     </div>
                 </div>
