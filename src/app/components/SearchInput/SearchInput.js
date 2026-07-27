1999d3cfd5e205e981f80f55a91c7b4eb7ee3253
Mostafa <msari808755@gmail.com>
2026-07-27 05:41:34 -0700

    feat: HandlingApiResponseOnSearchingProducts

--- src/app/components/SearchInput/SearchInput.js
+++ src/app/components/SearchInput/SearchInput.js
@@ -4,33 +4,60 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import CircleLoading from "../CircleLoading/CircleLoading";
+import { useClickAway } from "react-use";
+import { useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const SearchInput = () => {
+    const ref = useRef();
     const [value , setValue] = useState(null);
-    const [isSeaching , setIsSearching] = useState(true);
+    const [isSeaching , setIsSearching] = useState(false);
     const [result , setResult] = useState(null);
-    const [loading , setLoading] = useState(true);
-    /* useEffect(()=>{
+    const [loading , setLoading] = useState(false);
+    const [error , setError] = useState(false);
+    useClickAway(ref , ()=>{
+        setValue(null);
+        setIsSearching(false);
+    })
+    useEffect(()=>{
         const SearchData = async () => {
             setLoading(true);
-            fetch(`${NEXT_PUBLIC_PRODUCTS_API_URL}/products/search?q=${value}&limit=10&skip=0&select=title,price,image`)
+            fetch(`${process.env.NEXT_PUBLIC_PRODUCTS_API_URL}/products/search?q=${value}`)
             .then(res => res.json())
             .then(data => setResult(data))
-            .catch(()=>{setResult(null)})
+            .catch(()=>{setError(true)})
             .finally(()=>{setLoading(false)})
+            console.log(result);
         };
         SearchData();
-    },[value]) */
+    },[value])
     return (
         <div className="flex flex-col gap-[10px] relative">
             <div className="w-full relative">
-                <input  className="bg-gray-100 border-[1px] border-gray-300 w-[200px] md:w-[520px] rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
+                <input onChange={(e)=>{setValue(e.target.value);setIsSearching(true)}} className="bg-gray-100 border-[1px] border-gray-300 w-[200px] md:w-[520px] rounded-lg text-right p-[5px] outline-none" type="text" placeholder="جستجو در محصولات ..." />
                 <button className="absolute left-[5px] text-gray-400 pt-[3px] hover:text-black h-full cursor-pointer"><FontAwesomeIcon icon={faSearch} /></button>
             </div>
             {isSeaching ?
-                <div className="w-full h-[200px] bg-white border-[1px] border-gray-400 border-solid rounded-md p-[10px] overflow-scroll scrollbar-none flex flex-col gap-[5px] absolute mt-[50px] z-[994]">
+                <div ref={ref} className="w-full h-[200px] bg-white border-[1px] border-gray-400 border-solid rounded-md p-[10px] overflow-scroll scrollbar-none flex flex-col gap-[5px] absolute mt-[50px] z-[994]">
                     {/* if loading ? loading component (circle loading) : result items map  */}
-                    {loading ? <CircleLoading/> : null}
+                    {loading ? <CircleLoading/>
+                        :
+                        result.products.length !==0 ? 
+                        result?.products.map((item)=>(
+                            <Link onClick={()=>{setIsSearching(false)}} key={item?.id} href={`/products/${item?.id}`} className="w-full flex justify-between p-[5px]">
+                                <Image src={item?.images[0]} alt="resultImg" width={100} height={100} quality={100} className="w-[50px] h-auto md:w-[75px] lg:w-[100px]"/>
+                                <div className="flex gap-[5px] text-[10px] text-gray-600 md:text-[16px] lg:text-[20px]">
+                                    <p>{item?.title}</p>
+                                    <p>{item?.price}</p>
+                                </div>
+                            </Link>
+                        ))
+                        :
+                        <div className="w-full h-full flex justify-center items-center text-[10px] text-gray-600 md:text-[16px] lg:text-[20px]">
+                            <p>نتیجه ای یافت نشد</p>
+                        </div>
+                    }
                 </div>
                 :
                 null
