95795e6658a73a5d3552416555492f38593a3ec6
Mostafa <msari808755@gmail.com>
2026-07-29 09:04:02 -0700

    refactor: refactoringTheCodeAndStyleOfTheCategorySectionOnSubnavbar

--- src/app/components/Navbar/SubNavbar/Subnav.js
+++ src/app/components/Navbar/SubNavbar/Subnav.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
-
+// category motion variant
 const motionVariant = {
     initial:{
         display:"none",
         scale:0.3,
-        transformOrigin:"top right",
+        transformOrigin:"50% 0%",
     },
     animate:{
         display:"flex",
@@ -22,67 +22,39 @@ const motionVariant = {
 
 const SubNavbar = () => {
     // states for SubNavbar category menu
-    const [isHovered , setIsHovered] = useState(false);
-    const [isSection , setIsSection] = useState(false);
-    const [clothes , setClothes]  = useState(false);
-    const [digital , setDigital]  = useState(false);
-    const [valubles , setValubles]  = useState(false);
-
-    const ClothesSec = () =>{
-        setDigital(false);
-        setValubles(false);
-        setClothes(true);
-    };
-    const ValublesSec = () =>{
-        setDigital(false);
-        setValubles(true);
-        setClothes(false);
-    };
-    const DigitalSec = () =>{
-        setDigital(true);
-        setValubles(false);
-        setClothes(false);
-    };
-    const ResetAllSec = ()=>{
-        setDigital(false);
-        setValubles(false);
-        setClothes(false);
-    }
+    const [isHovered , setIsHovered] = useState(false); //category display status
+    const [selectedCategory , setSelectedCategory] = useState(null); //selected category
+    const CategoryList = [{title:"کیف زنانه",link:"womens-bags",cate:"clothes"},{title:"لباس زنانه",link:"womens-dresses",cate:"clothes"},{title:"زیورآلات زنانه",link:"womens-jewellery",cate:"clothes"}
+        ,{title:"کفش زنانه",link:"womens-shoes",cate:"clothes"},{title:"لباس مردانه",link:"mens-shirts",cate:"clothes"},{title:"کفش مردانه",link:"mens-shoes",cate:"clothes"},{title:"ساعت مردانه",link:"mens-watches",cate:"clothes"}
+        ,{title:"عینک آفتابی",link:"sunglasses",cate:"clothes"},{title:"موبایل",link:"smartphones",cate:"digital"},{title:"تبلت",link:"tablets",cate:"digital"},{title:"لوازم جانبی موبایل",link:"mobile-accessories",cate:"digital"}
+        ,{title:"لپ تاپ",link:"laptops",cate:"digital"},{title:"مراقبت پوست",link:"skin-care",cate:"beauty"},{title:"آرایشی",link:"beauty",cate:"beauty"},{title:"عطر",link:"fragrances",cate:"beauty"}
+        ,{title:"لوازم آشپزخانه",link:"kitchen-accessories",cate:"kitchen"},{title:"موتورسیکلت",link:"motorcycle",cate:"vehicle"},{title:"خودرو سواری",link:"vehicle",cate:"vehicle"}
+    ]
 
     return (
-        <div className="w-screen h-[100px] hidden lg:flex justify-right items-center gap-[70px] pr-[100px] text-gray-600 relative">
-            <div><Link href={"/"} className="hover:text-gray-800">خانه</Link></div>
-            <div onMouseEnter={()=>{setIsHovered(true)}}>
-                <Link href={"/products"} className="hover:text-gray-800">دسته بندی ها</Link>
-                <motion.div onMouseLeave={()=>{setIsHovered(false);setIsSection(false);ResetAllSec();}} variants={motionVariant} initial={"initial"} animate={isHovered ? "animate" : "initial"} transition={"transition"} className={`absolute bg-white z-[999] flex-col gap-[10px] mt-[20px] p-[10px] border-solid border-gray-300 border-[1px] rounded-lg shadow-gray-400 shadow-sm`}>
-                    <motion.div className="flex flex-row gap-[10px]">
-                        <motion.div className="flex flex-col gap-[10px]">
-                            <motion.div onMouseEnter={()=>{ClothesSec();setIsSection(true)}} transition={{duration:0.1}} animate={clothes ? {borderBottom:"1px solid gray"} : {borderBottom:"none"}} className={`w-full`}><Link className="hover:text-gray-800" href={"/"}>پوشاک</Link></motion.div>
-                            <motion.div onMouseEnter={()=>{DigitalSec();setIsSection(true)}} transition={{duration:0.1}} animate={digital ? {borderBottom:"1px solid gray"} : {borderBottom:"none"}} className={`w-full`}><Link className="hover:text-gray-800" href={"/"}>دیجیتال</Link></motion.div>
-                            <motion.div onMouseEnter={()=>{ValublesSec();setIsSection(true)}} transition={{duration:0.1}} animate={valubles ? {borderBottom:"1px solid gray"} : {borderBottom:"none"}} className={`w-full`}><Link className="hover:text-gray-800" href={"/"}>زیورآلات</Link></motion.div>
-                        </motion.div>
-                        <motion.div className={`w-[200px] justify-center h-full ${isSection ? "flex" : "hidden"}`}>
-                            {clothes ? (<div className="flex flex-col gap-[10px]">
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>ژاکت</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>کفش</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>کلاه</Link></div>
-                            </div>) : null}
-                            {digital ? (<div className="flex flex-col gap-[10px]">
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>موبایل</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>کارت حافظه</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>ساعت</Link></div>
-                            </div>) : null}
-                            {valubles ? (<div className="flex flex-col gap-[10px]">
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>دست بند</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>انگشتر</Link></div>
-                                <div className="w-full"><Link className="hover:text-gray-800" href={"/"}>گردن بند</Link></div>
-                            </div>) : null}
-                        </motion.div>
-                    </motion.div>
+        <div onMouseLeave={()=>{setIsHovered(false);setSelectedCategory(null)}} className="w-screen h-[100px] hidden lg:flex justify-right items-center gap-[70px] pr-[100px] text-gray-600 relative">
+            <div onMouseEnter={()=>{setIsHovered(false);setSelectedCategory(null)}}><Link href={"/"} className="hover:text-gray-800">خانه</Link></div>
+            <div className="xl:hidden"><Link href={"/products"} className="hover:text-gray-800">دسته بندی ها</Link></div> {/* on lg screen */}
+            <div className="hidden xl:block">
+                <Link onMouseOver={()=>{setIsHovered(true)}} href={"/products"} className="hover:text-gray-800">دسته بندی ها</Link>
+                <motion.div variants={motionVariant} initial={"initial"} animate={isHovered ? "animate" : "initial"} transition={"transition"} className={`absolute w-[80%] h-[500px] flex gap-[10px] mt-[20px] p-[10px] border-solid border-gray-300 border-[1px] bg-white z-[9999] rounded-xl`}>
+                    <div className="flex flex-col flex-1 justify-center gap-[20px] pr-[30px] gap text-gray-500 text-[20px]">
+                        <div><Link onMouseEnter={()=>{setSelectedCategory("clothes")}} href={"/"} className="hover:text-gray-700">پوشاک</Link></div>
+                        <div><Link onMouseEnter={()=>{setSelectedCategory("beauty")}}  href={"/"} className="hover:text-gray-700">آرایشی و بهداشتی</Link></div>
+                        <div><Link onMouseEnter={()=>{setSelectedCategory("kitchen")}} href={"/"} className="hover:text-gray-700">وسایل پخت و پز</Link></div>
+                        <div><Link onMouseEnter={()=>{setSelectedCategory("vehicle")}} href={"/"} className="hover:text-gray-700">وسایل نقلیه</Link></div>
+                        <div><Link onMouseEnter={()=>{setSelectedCategory("digital")}} href={"/"} className="hover:text-gray-700">وسایل دیجیتال</Link></div>
+                    </div>
+                    <div className="flex flex-col flex-3 gap-[20px] pr-[30px] justify-center text-[20px]">
+                        {/* first need to create new component to show category requested api result */}
+                        {CategoryList.map((item)=>( 
+                            <div key={Math.floor(Math.random()*1000000)} className={`${selectedCategory === item.cate ? "block" : "hidden"}`}><Link href={"/"} className="hover:text-gray-700">{item.title}</Link></div>
+                        ))}
+                    </div>
                 </motion.div>
             </div>
-            <div><Link href={"/"} className="hover:text-gray-800">وبلاگ</Link></div>
-            <div><Link href={"/"} className="hover:text-gray-800">درباره ما</Link></div>
+            <div onMouseMove={()=>{setIsHovered(false)}}><Link href={"/"} className="hover:text-gray-800">وبلاگ</Link></div>
+            <div onMouseMove={()=>{setIsHovered(false)}}><Link href={"/"} className="hover:text-gray-800">درباره ما</Link></div>
         </div>
     );
 }
