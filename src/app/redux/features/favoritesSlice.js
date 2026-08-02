9519e1b634a244c25f09d4050c1dfad31c6ff3b5
Mostafa <msari808755@gmail.com>
2026-08-02 02:21:22 -0700

    refactor: changeFavoritesSliceBehavior

--- src/app/redux/features/favoritesSlice.js
+++ src/app/redux/features/favoritesSlice.js
@@ -9,12 +9,15 @@ const FavoritesSlice = createSlice({
     name:"FavoritesSlice",
     initialState,
     reducers : {
-        toggleFavorite : (state,action)=>{  // this reducer will toggle favorites products 
-            {state.favorites.includes(action.payload) ? state.favorites.splice((item)=>{state.favorites.findIndex(action.payload)},1) : state.favorites.push(action.payload)}
+        AddFavorite : (state,action)=>{  // this reducer will toggle favorites products 
+            state.favorites.push(action.payload)
         },
+        removeFavorite : (state,action)=>{
+            state.favorites.splice(state.favorites.findIndex(item => item.id === action.payload.id),1)
+        }
     }
 })
 
 
 export default FavoritesSlice.reducer;
-export const {toggleFavorite} = FavoritesSlice.actions;
+export const {AddFavorite,removeFavorite} = FavoritesSlice.actions;
