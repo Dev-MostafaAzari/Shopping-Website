f2659ff7eee6766a42650a4290947490ae7fcb91
Mostafa <msari808755@gmail.com>
2026-08-02 03:28:55 -0700

    feat: retrievingDataFromLocalStorage

--- src/app/redux/features/favoritesSlice.js
+++ src/app/redux/features/favoritesSlice.js
@@ -1,8 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+//geting localStorage Data
+const LoadFavorites = ()=>{
+    if (typeof window === "undefined")
+    {
+        return [];
+    }
+    const {favorites} = JSON.parse(localStorage.getItem("products") || "{}")
+
+    return {
+        favorites : favorites || [],
+    }
+}
+const favoritesData = LoadFavorites();  // contains favorite Products
 
 const initialState = {
-    favorites : []
+    favorites : favoritesData.favorites,    // setLocatStorage data 
 };
 
 const FavoritesSlice = createSlice({
@@ -10,10 +23,10 @@ const FavoritesSlice = createSlice({
     initialState,
     reducers : {
         AddFavorite : (state,action)=>{  // this reducer will toggle favorites products 
-            state.favorites.push(action.payload)
+            state.favorites?.push(action.payload)
         },
         removeFavorite : (state,action)=>{
-            state.favorites.splice(state.favorites.findIndex(item => item.id === action.payload.id),1)
+            state.favorites?.splice(state.favorites.findIndex(item => item.id === action.payload.id),1)
         }
     }
 })
