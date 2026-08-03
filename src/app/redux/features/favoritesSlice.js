ebc71b17547d1c0b400a509a3c8ccf0ad0e08084
Mostafa <msari808755@gmail.com>
2026-08-03 08:16:44 -0700

    refactor: RemoveTheImpactOfTheInitialLocalStorageValueOnFavoritesState

--- src/app/redux/features/favoritesSlice.js
+++ src/app/redux/features/favoritesSlice.js
@@ -1,21 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-//geting localStorage Data
-const LoadFavorites = ()=>{
-    if (typeof window === "undefined")
-    {
-        return [];
-    }
-    const {favorites} = JSON.parse(localStorage.getItem("products") || "{}")
-
-    return {
-        favorites : favorites || [],
-    }
-}
-const favoritesData = LoadFavorites();  // contains favorite Products
-
 const initialState = {
-    favorites : favoritesData.favorites,    // setLocatStorage data 
+    favorites :[], 
+    start:0,
+    end:9,
+    activePage:0,
 };
 
 const FavoritesSlice = createSlice({
@@ -27,10 +16,18 @@ const FavoritesSlice = createSlice({
         },
         removeFavorite : (state,action)=>{
             state.favorites?.splice(state.favorites.findIndex(item => item.id === action.payload.id),1)
+        },
+        setFavPage : (state,action)=>{
+            state.start = action.payload*10;
+            state.end = (action.payload*10)+9;
+            state.activePage = action.payload;
+        },
+        setFavorites : (state,action)=>{
+            state.favorites = action.payload;
         }
     }
 })
 
 
 export default FavoritesSlice.reducer;
-export const {AddFavorite,removeFavorite} = FavoritesSlice.actions;
+export const {AddFavorite,removeFavorite,setFavPage,setFavorites} = FavoritesSlice.actions;
